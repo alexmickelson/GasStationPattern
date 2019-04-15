da9c98a29954c9f087267f55561ee4288dd47cfa
alexmickelson96 <alexmickelson96@gmail.com>
2019-04-15 09:24:56 -0600

    working on currency test

--- src/TestCashCurrency.java
+++ src/TestCashCurrency.java
@@ -2,6 +2,8 @@ import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
 import org.jmock.integration.junit4.JUnit4Mockery;
 import org.jmock.Expectations;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
@@ -10,14 +12,29 @@ import java.security.InvalidParameterException;
 
 
 public class TestCashCurrency {
-    Mockery context = new JUnit4Mockery();
+    static Mockery context;
+    static ICustomer mockCustomer;
+    static double price85;
+    static double price87;
+    static double price89;
 
+    @BeforeClass
+    public static void setup(){
+        context = new JUnit4Mockery();
+        mockCustomer = context.mock(ICustomer.class);
+    }
+
+    @Before
+    public static void beforeTest(){
+        price85 = 2.0;
+        price87 = 2.7;
+        price89=2.9;
+    }
 
     @Test
     public void InvalidMoneyTypeInCustomer_ThrowsError(){
         double price = 2.0;
 
-        ICustomer mockCustomer = context.mock(ICustomer.class);
         var credit = CurrencyEnum.CREDIT_CARD;
 
         context.checking(new Expectations(){{
@@ -32,5 +49,9 @@ public class TestCashCurrency {
         //new CashCurrencyHandler(mockCustomer, price, price, price);
     }
 
+    @Test
+    public void amountCharged_returnsPriceTimesGasAmt(){
+        CashCurrencyHandler currencyHandler = new CashCurrencyHandler(Customer, price85, price87, price89);
+    }
 
 }
