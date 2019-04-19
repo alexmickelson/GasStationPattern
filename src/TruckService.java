10ffc4acf217149b63388299d5f4f491f8564226
kyler daybell <relyk.photography.kyler@gmail.com>
2019-04-19 10:30:05 -0600

    Truck Serviece should work now

--- src/TruckService.java
+++ src/TruckService.java
@@ -1,8 +1,11 @@
-
 import java.util.LinkedList;
+import java.util.Random;
 
 public class TruckService implements ITruckService {
     private LinkedList<TankServiceSchedule> tankList;
+    private int tickscurrent;
+    private int minimumWaitTime;
+    private double PumpRate;
 
     TruckService(ITimeObservable time){
         tankList = new LinkedList<>();
@@ -11,7 +14,13 @@ public class TruckService implements ITruckService {
 
     @Override
     public void CallTruck(ITank tank, double amt) {
-        TankServiceSchedule TankServiceSchedule = new TankServiceSchedule(tank, 10, amt);
+        //generate a random number between 50 and 250
+        Random rand = new Random();
+        //add that to the current ticks
+        int n = rand.nextInt(200);
+        //add 50-250  ms to the currnt time b
+        n = n + minimumWaitTime + tickscurrent;
+        TankServiceSchedule TankServiceSchedule = new TankServiceSchedule(tank, n, amt);
 
         for (var tankService : tankList) {
             if (tankService.tank != tank) {
@@ -22,12 +31,22 @@ public class TruckService implements ITruckService {
 
     @Override
     public void update(int ticks) {
+        this.tickscurrent = ticks;
         for (var tankService : tankList) {
             if (tankService.serviceTime >= ticks) {
-                //fill tank
+                tankService.tank.GiveGasToTank(PumpRate);
             }
 
         }
     }
+
+    public void SetMinimumWaitTime(int min){
+        minimumWaitTime = min;
+    }
+
+    public void SetPumpRate(double rate){
+        PumpRate = rate;
+    }
+
 }
 
