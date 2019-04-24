6e1d3340f771745d6d8f947c0947ee53bee177a7
alexmickelson96 <alexmickelson96@gmail.com>
2019-04-23 18:17:51 -0600

    sets on all properties

--- src/SwingStationFrame.java
+++ src/SwingStationFrame.java
@@ -2,8 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class SwingStationFrame extends JFrame {
+    private SwingPumps pumps;
+    private SwingTanks tanks;
+    private SwingStatsGeneral stats;
+    private SwingButtons buttons;
 
     public SwingStationFrame(){
+        pumps = new SwingPumps();
+        tanks = new SwingTanks();
+        stats = new SwingStatsGeneral();
+        buttons = new SwingButtons();
 
         setLayout(new GridBagLayout());
         var c = new GridBagConstraints();
@@ -11,15 +19,15 @@ public class SwingStationFrame extends JFrame {
         c.gridx=0;
         c.gridy=0;
         c.gridheight=3;
-        add(new SwingPumps(), c);
+        add(pumps, c);
         c.gridheight=1;
         c.gridx=1;
-        add(new SwingTanks(), c);
+        add(tanks, c);
         c.gridy=1;
-        add(new SwingStatsGeneral(), c);
+        add(stats, c);
         c.gridy=2;
 
-        add(new SwingButtons(), c);
+        add(buttons, c);
 
 
 
